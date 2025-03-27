2b461dc45a84e11eac77cccb5a4ff564565461fa
antoine <leng.antoine@gmail.com>
2025-03-27 13:31:20 +0100

    fonctionnalité modifier les informations du compte terminé CSS à faire

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\EditProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -42,5 +46,34 @@ class ProfileController extends AbstractController
     public function admin(): Response
     {
         return $this->render('profile/admin.html.twig');
+    }
+
+    #[Route('/profile/edit', name: 'app_profile_edit')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function edit(
+        Request $request,
+        UserInterface $user,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
+    ): Response {
+        $form = $this->createForm(EditProfileFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+            }
+
+            $em->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès !');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/EditProfileFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\User;
+
+class EditProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('email', EmailType::class, ['label' => 'Email'])
+            ->add('pseudo', TextType::class, ['label' => 'Pseudo'])
+            ->add('password', PasswordType::class, [
+                'label' => 'Nouveau mot de passe',
+                'required' => false, 
+                'mapped' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => User::class]);
+    }
+}
