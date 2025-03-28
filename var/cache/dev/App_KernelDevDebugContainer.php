d8e50a726b10f200a357016773b4cccef9caa303
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-03-28 10:31:25 +0100

    premier qcm de test, migration

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerXT9EWCx\App_KernelDevDebugContainer([
     'container.build_hash' => 'XT9EWCx',
-    'container.build_id' => 'ef49f6e1',
-    'container.build_time' => 1743149738,
+    'container.build_id' => 'c1cbe82e',
+    'container.build_time' => 1743154081,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerXT9EWCx');
