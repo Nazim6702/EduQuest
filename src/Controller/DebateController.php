06feef807cfa3c83dbf68528b28d6320e13cbc72
sarah <sarahlakrouz@gmail.com>
2025-04-02 16:00:00 +0200

    création du formulaire relié a DebateMessage

--- src/Controller/DebateController.php
+++ src/Controller/DebateController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\DebateMessage;
+use App\Form\DebateMessageType;
+use App\Repository\DebateMessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class DebateController extends AbstractController
+class DebateController extends AbstractController
 {
-    #[Route('/debate', name: 'app_debate')]
-    public function index(): Response
+    #[Route('/debat', name: 'app_debate')]
+    public function index(Request $request, EntityManagerInterface $em, DebateMessageRepository $repo): Response
     {
+        $message = new DebateMessage();
+        $form = $this->createForm(DebateMessageType::class, $message);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setAuthor($this->getUser());
+            $message->setCreatedAt(new \DateTimeImmutable());
+            $message->setLikes(0);
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute('app_debate');
+        }
+
+        $messages = $repo->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('debate/index.html.twig', [
-            'controller_name' => 'DebateController',
+            'form' => $form->createView(),
+            'messages' => $messages
         ]);
     }
 }
