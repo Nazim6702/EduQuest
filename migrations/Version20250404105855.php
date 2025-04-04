b8e3102c9d179a6f299ca7043fa89b416007b49d
aminaaparis1 <amina.zouane@etu.univ-paris1.fr>
2025-04-04 12:59:56 +0200

    migration

--- migrations/Version20250404105855.php
+++ migrations/Version20250404105855.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250404093244 extends AbstractMigration
+final class Version20250404105855 extends AbstractMigration
 {
     public function getDescription(): string
     {
